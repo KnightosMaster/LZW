47b682424c6bb1f5f727e62ca58d495dbf9b7ab9
Knightos <knightosnev@gmail.com>
2020-01-21 18:33:10 +0100

    Timing added

--- src/main/java/com/tvzs/lzw/Main.java
+++ src/main/java/com/tvzs/lzw/Main.java
@@ -2,12 +2,23 @@ package com.tvzs.lzw;
 
 public class Main {
 
+    private static final LZWEncoder LZW_ENCODER = new LZWEncoder();
+    private static final LZWDecoder LZW_DECODER = new LZWDecoder();
+
     public static void main(String[] args) {
-        LZWEncryptor encryptor = new LZWEncryptor();
-        encryptor.encryptWithoutAlphabet("ababcababa");
-        LZWDecryptor decryptor = new LZWDecryptor();
-        String[] alphabet = {"a", "b", "c"};
-        decryptor.decrypt("3 000001011010011111", alphabet);
+        encryptAndDecrypt("ababcababa");
+    }
+
+    private static void encryptAndDecrypt(String input) {
+        long lzwStartTime = System.nanoTime();
+        LZW_ENCODER.encryptWithoutAlphabet(input);
+        long lzwMidTime = System.nanoTime();
+        LZW_DECODER.decode(LZW_ENCODER.getDecodableString(), LZW_ENCODER.getAlphabet());
+        long lzwEndTime = System.nanoTime();
+        System.out.println("==LZW==\n" +
+                "Encryption took " + (lzwMidTime - lzwStartTime)/1e6 + " milliseconds\n" +
+                "Decoding took " + (lzwEndTime - lzwMidTime)/1e6 + " milliseconds\n" +
+                "Compressed/original ratio is " + LZW_ENCODER.getEfficiency() + "%\n");
     }
 
 }
